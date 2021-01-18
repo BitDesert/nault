daf82c50c42e59316ea18224f828a7e83efccda3
BitDesert <11021604+BitDesert@users.noreply.github.com>
2021-01-18 21:03:24 +0100

    fix linting

--- src/app/services/app-settings.service.ts
+++ src/app/services/app-settings.service.ts
@@ -128,7 +128,7 @@ export class AppSettingsService {
       settings = JSON.parse(settingsStore);
     }
     this.settings = Object.assign(this.settings, settings);
-    
+
     if (this.settings.language === null) {
       const browserLang = this.translate.getBrowserLang();
       if (this.translate.getLangs().includes(browserLang)) {
